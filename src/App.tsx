6943aaba852e68eb77ea6a732fbbe6f22106f3e8
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2026-09-01 14:57:00 +0545

    feat: implement booking form

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { ServiceListPage } from "./features/services/pages/ServiceListPage";
 import { AppNav } from "./components/AppNav";
 import { ServiceDetailsPage } from "./features/services/pages/ServiceDetailsPage";
+import { BookingPage } from "./features/booking/pages/BookingPage";
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Navigate to="/services" replace />} />
         <Route path="/services" element={<ServiceListPage />} />
          <Route path="/services/:serviceId" element={<ServiceDetailsPage/>} />
+          <Route path="/services/:serviceId/book" element={<BookingPage />} />
       </Routes>
     </>
   );
